338554a1f7a2bf250228ea86b1847c5573f78a23
Giovanni Delgado <giovanniyatze@gmail.com>
2020-04-05 18:10:00 -0500

    Add urls.py file definition

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from rest_framework_jwt.views import refresh_jwt_token, verify_jwt_token
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+# Main Page route
+    path('', admin.site.urls),
+# Django admin routes
+    path('admin/', admin.site.urls),   
+# Auth routes
+# path('auth/', include('accounts.auth_urls')),
+    path('api-token-refresh/', refresh_jwt_token, name='refresh-token'),
+    path('api-token-verify/', verify_jwt_token, name='verify-token'),
 ]
